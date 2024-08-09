c371f8cf95b0d5c89dc8bb04cfcd7f6f2b6f3d4f
Leo <88931222+Leo4Bey@users.noreply.github.com>
2024-08-09 12:47:03 +0300

    Add files via upload

---
+++ BankInfo.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+class BankaHesabi {
+    private:
+        int HesapNo;
+        std::string HesapSahibi;
+        float Bakiye;
+    public:
+        BankaHesabi(int no, std::string sahip, float baslangicBakiyesi) {
+            HesapNo = no;
+            HesapSahibi = sahip;
+            Bakiye = baslangicBakiyesi;
+        }
+
+        int getHesapNo() const {
+            return HesapNo;
+        }
+
+        std::string getHesapSahibi() const {
+            return HesapSahibi;
+        }
+
+        float getBakiye() const {
+            return Bakiye;
+        }
+
+        void paraCek(float miktar) {
+            if (miktar > 0 && miktar <= Bakiye) {
+                Bakiye -= miktar;
+                std::cout << HesapNo << " Nolu hesaptan" << miktar << " TL tutarında para çekildi güncel bakiye: " << Bakiye << " TL\n";
+            }
+            else if (miktar > Bakiye) {
+                std::cout << "Yetersiz bakiye çekebileceğin maksimum tutar " << Bakiye << " TL\n";
+            }
+            else {
+                std::cout << "pozitif bir değer girmeniz lazım";
+            }
+        }
+
+        void paraYatir(float miktar) {
+            if (miktar > 0) {
+                Bakiye += miktar;
+                std::cout << miktar << "tutarında bakiye " << HesapNo << " Nolu hesaba yatırıldı güncel bakiye: " << Bakiye << "TL\n";
+            }
+            else {
+                std::cout << "Geçersiz tutar";
+            }
+        }
+};
+
+
+int main() {
+    
+    BankaHesabi hesap(125488,"Leo",50.47);
+    std::cout << "Hesap bakiyesi: " << hesap.getBakiye() << "\n";
+    hesap.paraYatir(2587.47);
+    hesap.paraCek(145.47);
+    return 0;
+}
